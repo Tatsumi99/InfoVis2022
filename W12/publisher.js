2429f54451991301da82dd514510d0fbba2f2a76
Tatsumi99 <227x015x@stu.kobe-u.ac.jp>
2022-06-06 01:43:40 +0900

    Initial commit

--- W12/publisher.js
+++ W12/publisher.js
@@ -1,34 +1,75 @@
-window.addEventListener("load", function(){
-    var stage = d3.select("#demo_stage");
-    var svg = stage.append("svg")
-        .attr("width",200)
-        .attr("height",200);
-    var grid = [
-        [1,1,1,1,1],[2,2,2,2,2],[3,3,3,3,3],[4,4,4,4,4],[5,5,5,5,5]
-    ];
-    var dataArr = [
-        [3,3.5,4,5,3.5]
-       ,[5,4.0,2,3,4.0]
-    ];
-    var scale = d3.scaleLinear()
-        .domain([0,5]).range([0,90]);
-    var line = d3.line()
-        .x(function(d,i){return scale(d) * Math.sin(Math.PI*2/5 * i) + 100;})
-        .y(function(d,i){return -scale(d) * Math.cos(Math.PI*2/5 * i) + 100;})
+d3.csv("https://tatsumi99.github.io/InfoVis2022/W12/power.csv")
+    .then( data => {
+        data.forEach( d => { d.Height = +d.Height;});
+
+        var config = {
+            parent: '#drawing_region',
+            width: 512,
+            height: 512,
+            margin: {top:10, right:10, bottom:20, left:50}
+        };
+
+        const Barplot = new BarChart( config, data );
+        Barplot.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class BarChart {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 128,
+            margin: config.margin || {top:10, right:10, bottom:20, left:60}
+        }
+        this.data = data;
+        this.init();
+    }
+        init() {
+            let self = this;
+    
+            self.svg = d3.select( self.config.parent )
+                .attr('width', self.config.width)
+                .attr('height', self.config.height);
+    
+            self.chart = self.svg.append('g')
+                .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+    
+            self.grid = [
+                [1,1,1,1,1],[2,2,2,2,2],[3,3,3,3,3],[4,4,4,4,4],[5,5,5,5,5]
+            ];
+
+            self.scale = d3.scaleLinear()
+            .domain([0,5]).range([0,90]);
+
+            self.line = d3.line()
+            .x(function(d,i){return self.scale(self.data) * Math.sin(Math.PI*2/5 * i) + 100;})
+            .y(function(d,i){return self.scale(self.data) * Math.cos(Math.PI*2/5 * i) + 100;})
         
-    svg.selectAll("path.grid")
-        .data(grid)
+            
+
+        }    
+
+    update() {
+        let self = this;
+
+        self.chart.selectAll("text").remove();
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        self.svg.selectAll("path.grid")
+        .data(self.grid)
         .enter()
         .append("path")
-        .attr("d", function(d,i){return line(d)+"z";})
+        .attr("d", d  => self.line(d)+"z")
         .attr("stroke", "black")
         .attr("stroke-dasharray", "2");
-    svg.selectAll("path.data")
-        .data(dataArr)
-        .enter()
-        .append("path")
-        .attr("d", function(d,i){return line(d)+"z";})
-        .attr("stroke", function(d,i){return i ? "red": "blue";})
-        .attr("stroke-width", 2);
-    svg.selectAll("path").attr("fill", "none")
-},false);
+    }
+}
